10494b97f1e68306f44e0e4b4625fbdc90c7feb9
Chase <pd.van.lill@gmail.com>
2014-05-13 19:39:29 +0200

    Initial Commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/4e/c011353fc2da00131a25a5e7f97d77d0
@@ -0,0 +1,9 @@
+package src.scoringsystem.fabio;
+
+public class Test
+{
+	void blahblah()
+	{
+		
+	}
+}
---
+++ ScoringSystemJava/src/net/window/frames/admin/AdminFrame.java
@@ -0,0 +1,62 @@
+package net.window.frames.admin;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class AdminFrame extends JFrame
+{
+	
+	/**
+	 * 
+	 */
+    private static final long serialVersionUID = 4638510630518996374L;
+	private JPanel contentPane;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					AdminFrame frame = new AdminFrame();
+					frame.setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	/**
+	 * Create the frame.
+	 */
+	public AdminFrame()
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 680, 480);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblAdmin = new JLabel("Admin");
+		lblAdmin.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAdmin.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		lblAdmin.setBounds(101, 0, 471, 91);
+		contentPane.add(lblAdmin);
+
+	}
+}
