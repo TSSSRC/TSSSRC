b282a926114238fa9f63376fd89246ccb6c2cbc6
rexex <fabio.loreggian@gmail.com>
2014-05-20 17:13:25 +0200

    Added a new button
    lolly

--- ScoringSystemJava/src/net/util/KeyPad.java
+++ ScoringSystemJava/src/net/util/KeyPad.java
@@ -8,9 +8,12 @@ public class KeyPad extends JPanel
 	public KeyPad()
 	{
 		JButton btnNewButton = new JButton("New button1");
+		JButton btnNewButton2 = new JButton("New button2");
 		
 		btnNewButton.setBounds(58, 196, 52, 23);
+		btnNewButton2.setBounds(58, 56, 152, 23);
 		this.add(btnNewButton);
+		this.add(btnNewButton2);
 		this.setBounds(311, 141, 239, 160);
 	}
 }
